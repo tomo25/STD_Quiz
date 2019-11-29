119bb052c396f0fc50e2e540492e94e2d4dc64d1
tomo25 <bruiser4l.617@gmail.com>
2019-11-29 14:19:09 +0900

    add sample

--- contracts/STD_Quiz_1.sol
+++ contracts/STD_Quiz_1.sol
@@ -1,13 +1,12 @@
  pragma solidity ^0.5.0;
 
  contract STD_Function_public {
-     uint foo;
+     uint public foo;
      function manyArgs(
          uint a1, uint a2, uint a3, uint a4, uint a5, uint a6, 
          uint a7, uint a8, uint a9, uint a10, uint a11, uint a12, 
          uint a13, uint a14, uint a15, uint a16, uint a17
-         ) public returns(uint){
+         ) public {
              foo = a17;
-             return foo;
      }
  }
