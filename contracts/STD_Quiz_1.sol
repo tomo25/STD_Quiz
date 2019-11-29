42dd8aa4285ff492bd5e56a6562d042e5835250b
tomo25 <bruiser4l.617@gmail.com>
2019-11-29 14:34:43 +0900

    add sample

--- contracts/STD_Quiz_1.sol
+++ contracts/STD_Quiz_1.sol
@@ -1,12 +1,12 @@
  pragma solidity ^0.5.0;
 
-contract TestStackError {
-  event LogValue(uint);
-  function logArg(uint a1, uint a2, uint a3, uint a4,
-	uint a5, uint a6, uint a7, uint a8,
-	uint a9, uint a10, uint a11, uint a12,
-	uint a13, uint a14, uint a15, uint a16
-  ) public {
-    emit LogValue(a2);
-  }
-}
+ contract STD_Function_public {
+     uint public foo;
+     function manyArgs(
+         uint a1, uint a2, uint a3, uint a4, uint a5, uint a6, 
+         uint a7, uint a8, uint a9, uint a10, uint a11, uint a12, 
+         uint a13, uint a14, uint a15, uint a16, uint a17
+         ) public {
+             foo = a17;
+     }
+ }
